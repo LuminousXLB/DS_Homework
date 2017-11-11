ab9b7d91ebc21176d029c762f9a1f0fec3b43279
LuminousXLB <john980118@163.com>
2017-11-11 12:24:12 +0800

    7-3

--- 7_3/main.cpp
+++ 7_3/main.cpp
@@ -1,77 +1,80 @@
-// Homework07_3.cpp : ��������̨Ӧ�ó��������ڵ㡣
-//
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 #include "mySet.h"
-#define SIZE 15
 using namespace std;
 
+const int SIZE = 15;
+
 int randbet(int inf, int sup) {
-	return inf + rand()*(sup - inf + 1) / (RAND_MAX + 1);
+    return inf + rand() * (sup - inf + 1) / (RAND_MAX + 1);
 }
+
 
 double randbet(double inf, double sup) {
-	return inf + (double)rand()*(sup - inf + 1) / (RAND_MAX + 1);
+    return inf + (double) rand() * (sup - inf + 1) / (RAND_MAX + 1);
 }
+
 
 void randassign(int arr[], int n) {
-	for (int i = 0; i < n; i++) {
-		arr[i] = randbet(1, 12);
-	}
+    for (int i = 0; i < n; i++) {
+        arr[i] = randbet(1, 12);
+    }
 }
+
 
 void randassign(double arr[], int n) {
-	for (int i = 0; i < n; i++) {
-		arr[i] = randbet(1.0, 12.0);
-	}
+    for (int i = 0; i < n; i++) {
+        arr[i] = randbet(1.0, 12.0);
+    }
 }
 
+
 void randassign(char arr[], int n) {
-	for (int i = 0; i < n; i++) {
-		arr[i] = randbet('a', 'n');
-	}
+    for (int i = 0; i < n; i++) {
+        arr[i] = randbet('a', 'n');
+    }
 }
 
 
 int main() {
-	srand(time(NULL));
+    srand(time(NULL));
 
-	int a1[SIZE], a2[SIZE];
-	double a3[SIZE], a4[SIZE];
-	char a5[SIZE], a6[SIZE];
+    int a1[SIZE], a2[SIZE];
+    double a3[SIZE], a4[SIZE];
+    char a5[SIZE], a6[SIZE];
 
-	randassign(a1, SIZE);
-	randassign(a2, SIZE);
-	randassign(a3, SIZE);
-	randassign(a4, SIZE);
-	randassign(a5, SIZE);
-	randassign(a6, SIZE);
+    randassign(a1, SIZE);
+    randassign(a2, SIZE);
+    randassign(a3, SIZE);
+    randassign(a4, SIZE);
+    randassign(a5, SIZE);
+    randassign(a6, SIZE);
 
-	mySet<int> s1(a1, SIZE), s2(a2, SIZE);
-	mySet<double> s3(a3, SIZE), s4(a4, SIZE);
-	mySet<char> s5(a5, SIZE), s6(a6, SIZE);
+    mySet<int> s1(a1, SIZE), s2(a2, SIZE);
+    mySet<double> s3(a3, SIZE), s4(a4, SIZE);
+    mySet<char> s5(a5, SIZE), s6(a6, SIZE);
 
-	cout << s1 << endl;
-	cout << s2 << endl;
-	cout << s1 + s2 << endl;
-	cout << s1 - s2 << endl;
-	cout << s1*s2 << endl;
-	cout << endl;
+    cout << s1 << endl;
+    cout << s2 << endl;
+    cout << s1 + s2 << endl;
+    cout << s1 - s2 << endl;
+    cout << s1 * s2 << endl;
+    cout << endl;
 
-	cout << s3 << endl;
-	cout << s4 << endl;
-	cout << s3 + s4 << endl;
-	cout << s3 - s4 << endl;
-	cout << s3*s4 << endl;
-	cout << endl;
+    cout << s3 << endl;
+    cout << s4 << endl;
+    cout << s3 + s4 << endl;
+    cout << s3 - s4 << endl;
+    cout << s3 * s4 << endl;
+    cout << endl;
 
-	cout << s5 << endl;
-	cout << s6 << endl;
-	cout << s5 + s6 << endl;
-	cout << s5 - s6 << endl;
-	cout << s5*s6 << endl;
-	cout << endl;
+    cout << s5 << endl;
+    cout << s6 << endl;
+    cout << s5 + s6 << endl;
+    cout << s5 - s6 << endl;
+    cout << s5 * s6 << endl;
+    cout << endl;
 
-	return 0;
+    return 0;
 }
--- 7_3/mySet.h
+++ 7_3/mySet.h
@@ -3,182 +3,207 @@
 using namespace std;
 
 int max(int a, int b) {
-	return (a > b) ? a : b;
+    return (a > b) ? a : b;
 }
 
-template <class T> class mySet;
-template <class T> ostream & operator<<(ostream & os, const mySet<T> & pr);
-template <class T> mySet<T> operator+(const mySet<T>& seq1, const mySet<T>& seq2);
-template <class T> mySet<T> operator*(const mySet<T>& seq1, const mySet<T>& seq2);
-template <class T> mySet<T> operator-(const mySet<T>& seq1, const mySet<T>& seq2);
 
 template <class T>
-class mySet
-{
-	friend ostream & operator<<<>(ostream & os, const mySet<T> & pr);
-	friend mySet<T> operator+<>(const mySet<T> & seq1, const mySet<T> & seq2);
-	friend mySet<T> operator*<>(const mySet<T> & seq1, const mySet<T> & seq2);
-	friend mySet<T> operator-<>(const mySet<T> & seq1, const mySet<T> & seq2);
+class mySet;
+template <class T>
+ostream& operator<<(ostream& os, const mySet<T>& pr);
+
+template <class T>
+mySet<T> operator+(const mySet<T>& seq1, const mySet<T>& seq2);
+
+template <class T>
+mySet<T> operator*(const mySet<T>& seq1, const mySet<T>& seq2);
+
+template <class T>
+mySet<T> operator-(const mySet<T>& seq1, const mySet<T>& seq2);
+
+template <class T>
+class mySet {
+    friend ostream & operator<<<>(ostream & os, const mySet<T> &pr);
+    friend mySet<T> operator+<>(const mySet<T> &seq1, const mySet<T> &seq2);
+    friend mySet<T> operator*<>(const mySet<T> &seq1, const mySet<T> &seq2);
+    friend mySet<T> operator-<>(const mySet<T> &seq1, const mySet<T> &seq2);
 public:
-	mySet & operator=(const mySet & other);
-	T & operator[](const int & index);
-	const T & operator[](const int & index) const;
+    mySet& operator=(const mySet& other);
+    T& operator[](const int& index);
+    const T& operator[](const int& index) const;
 
-	mySet(T array[], int n);
-	mySet(const mySet & other);
-	~mySet();
+    mySet(T array[], int n);
+    mySet(const mySet& other);
+    ~mySet();
 private:
-	T * seq;
-	int scale;
+    T *seq;
+    int scale;
 
-	void InsertSort(T sort[], int n);
-	int duplicateRemoval(T seq[], int n);
+    void InsertSort(T sort[], int n);
+    int duplicateRemoval(T seq[], int n);
 };
 
-template<class T>
-mySet<T> & mySet<T>::operator=(const mySet & other) {
-	if (this != &other) {
-		delete[] seq;
-		scale = other.scale;
-		seq = new T[scale];
-		for (int i = 0; i < scale; i++) {
-			seq[i] = other.seq[i];
-		}
-	}
-	return *this;
+template <class T>
+mySet<T>& mySet<T>::operator=(const mySet& other) {
+    if (this != &other) {
+        delete[] seq;
+        scale = other.scale;
+        seq = new T[scale];
+        for (int i = 0; i < scale; i++) {
+            seq[i] = other.seq[i];
+        }
+    }
+    return *this;
 }
 
-template<class T>
-T & mySet<T>::operator[](const int & index) {
-	return seq[index];
+
+template <class T>
+T& mySet<T>::operator[](const int& index) {
+    return seq[index];
 }
 
-template<class T>
-const T & mySet<T>::operator[](const int & index) const {
-	return seq[index];
+
+template <class T>
+const T& mySet<T>::operator[](const int& index) const {
+    return seq[index];
 }
+
 
 template <class T>
 mySet<T>::mySet(T array[], int n) {
-	InsertSort(array, n);
-	scale = duplicateRemoval(array, n);
-	seq = new T[scale];
-	for (int i = 0; i < scale; i++) {
-		seq[i] = array[i];
-	}
+    InsertSort(array, n);
+    scale = duplicateRemoval(array, n);
+    seq = new T[scale];
+    for (int i = 0; i < scale; i++) {
+        seq[i] = array[i];
+    }
 }
 
-template<class T>
-mySet<T>::mySet(const mySet & other) {
-	scale = other.scale;
-	seq = new T[scale];
-	for (int i = 0; i < scale; i++) {
-		seq[i] = other.seq[i];
-	}
+
+template <class T>
+mySet<T>::mySet(const mySet& other) {
+    scale = other.scale;
+    seq = new T[scale];
+    for (int i = 0; i < scale; i++) {
+        seq[i] = other.seq[i];
+    }
 }
+
 
 template <class T>
 mySet<T>::~mySet() {
-	delete[] seq;
+    delete[] seq;
 }
 
-template<class T>
+
+template <class T>
 void mySet<T>::InsertSort(T sort[], int n) {
-	T temp;
-	int k, i;
+    T temp;
+    int k, i;
 
-	for (k = 1; k < n; k++) {
-		temp = sort[k];
-		for (i = k; i > 0 && temp <= sort[i - 1]; i--) {
-			sort[i] = sort[i - 1];
-		}
-		sort[i] = temp;
-	}
+    for (k = 1; k < n; k++) {
+        temp = sort[k];
+        for (i = k; i > 0 && temp <= sort[i - 1]; i--) {
+            sort[i] = sort[i - 1];
+        }
+        sort[i] = temp;
+    }
 }
+
 
 template <class T>
 int mySet<T>::duplicateRemoval(T seq[], int n) {
-	int i = 1, step = 1;
-	for (i = 1; i < n; i++) {
-		if (seq[i] == seq[i - step]) {
-			step++;
-		} else {
-			seq[i - step + 1] = seq[i];
-		}
-	}
-	n -= --step;
-	return n;
+    int i = 1, step = 1;
+
+    for (i = 1; i < n; i++) {
+        if (seq[i] == seq[i - step]) {
+            step++;
+        } else {
+            seq[i - step + 1] = seq[i];
+        }
+    }
+    n -= --step;
+    return n;
 }
 
+
 template <class T>
-ostream & operator<<(ostream & os, const mySet<T> & pr) {
-	char span = '\t';
-	for (int i = 0; i < pr.scale; i++) {
-		os << pr[i] << span;
-	}
-	return os;
+ostream& operator<<(ostream& os, const mySet<T>& pr) {
+    char span = '\t';
+
+    for (int i = 0; i < pr.scale; i++) {
+        os << pr[i] << span;
+    }
+    return os;
 }
+
 
 template <class T>
 mySet<T> operator+(const mySet<T>& seq1, const mySet<T>& seq2) {
-	T * res = new T[max(seq1.scale, seq2.scale)];
-	int i = 0, j = 0, k = 0;
-	while (i < seq1.scale&&j < seq2.scale) {
-		if (seq1[i] < seq2[j]) {
-			i++;
-		} else if (seq1[i] > seq2[j]) {
-			j++;
-		} else {
-			res[k] = seq1[i];
-			i++, j++, k++;
-		}
-	}
+    T *res = new T[max(seq1.scale, seq2.scale)];
+    int i = 0, j = 0, k = 0;
 
-	return mySet<T>(res, k);
+    while (i < seq1.scale && j < seq2.scale) {
+        if (seq1[i] < seq2[j]) {
+            i++;
+        } else if (seq1[i] > seq2[j]) {
+            j++;
+        } else {
+            res[k] = seq1[i];
+            i++, j++, k++;
+        }
+    }
+
+    return mySet<T>(res, k);
 }
+
 
 template <class T>
 mySet<T> operator*(const mySet<T>& seq1, const mySet<T>& seq2) {
-	T * res = new T[seq1.scale + seq2.scale];
-	int i = 0, j = 0, k = 0;
-	while (i < seq1.scale&&j < seq2.scale) {
-		if (seq1[i] < seq2[j]) {
-			res[k] = seq1[i];
-			i++;
-		} else if (seq1[i] > seq2[j]) {
-			res[k] = seq2[j];
-			j++;
-		} else {
-			res[k] = seq1[i];
-			i++, j++;
-		}
-		k++;
-	}
-	for (; i < seq1.scale; i++, k++) {
-		res[k] = seq1[i];
-	}
-	for (; j < seq2.scale; j++, k++) {
-		res[k] = seq2[j];
-	}
-	return mySet<T>(res, k);
+    T *res = new T[seq1.scale + seq2.scale];
+    int i = 0, j = 0, k = 0;
+
+    while (i < seq1.scale && j < seq2.scale) {
+        if (seq1[i] < seq2[j]) {
+            res[k] = seq1[i];
+            i++;
+        } else if (seq1[i] > seq2[j]) {
+            res[k] = seq2[j];
+            j++;
+        } else {
+            res[k] = seq1[i];
+            i++, j++;
+        }
+        k++;
+    }
+    for ( ; i < seq1.scale; i++, k++) {
+        res[k] = seq1[i];
+    }
+    for ( ; j < seq2.scale; j++, k++) {
+        res[k] = seq2[j];
+    }
+    return mySet<T>(res, k);
 }
+
 
 template <class T>
 mySet<T> operator-(const mySet<T>& seq1, const mySet<T>& seq2) {
-	T * res = new T[max(seq1.scale, seq2.scale)];
-	int i = 0, j = 0, k = 0;
-	while (i < seq1.scale&&j < seq2.scale) {
-		if (seq1[i] < seq2[j]) {
-			res[k] = seq1[i];
-			i++; k++;
-		} else if (seq1[i] > seq2[j]) {
-			j++;
-		} else {
-			i++, j++;
-		}
-	}
-	for (; i < seq1.scale; i++, k++) {
-		res[k] = seq1[i];
-	}
-	return mySet<T>(res, k);
+    T *res = new T[max(seq1.scale, seq2.scale)];
+    int i = 0, j = 0, k = 0;
+
+    while (i < seq1.scale && j < seq2.scale) {
+        if (seq1[i] < seq2[j]) {
+            res[k] = seq1[i];
+            i++;
+            k++;
+        } else if (seq1[i] > seq2[j]) {
+            j++;
+        } else {
+            i++, j++;
+        }
+    }
+    for ( ; i < seq1.scale; i++, k++) {
+        res[k] = seq1[i];
+    }
+    return mySet<T>(res, k);
 }
