1eed2ccc7cbee70073c36c2a2985f07cbfde0568
LuminousXLB <john980118@163.com>
2017-11-28 02:08:23 +0800

    8-239remove?

--- 8_2,3,9/BinarySearchTree.h
+++ 8_2,3,9/BinarySearchTree.h
@@ -1,12 +1,16 @@
 #pragma once
+#include <stack>
+
+using namespace std;
+
 template <class Type>
 class BinarySearchTree {
  private:
-  struct BinaryNode  //二叉树结点结构
+  struct BinaryNode  // 二叉树结点结构
   {
-    Type data;          //节点键值
-    BinaryNode *left;   //指向左孩子的指针
-    BinaryNode *right;  //指向右孩子的指针
+    Type data;          // 节点键值
+    BinaryNode *left;   // 指向左孩子的指针
+    BinaryNode *right;  // 指向右孩子的指针
     BinaryNode(const Type &thedata, BinaryNode *lt, BinaryNode *rt)
         : data(thedata), left(lt), right(rt) {}
   };
@@ -18,67 +22,108 @@ class BinarySearchTree {
   bool find(const Type &x) const;
   void insert(const Type &x);
   void remove(const Type &x);
+  void midOrder() const {
+    if (root != NULL) {
+      cout << "\nmidOrder\t";
+      midOrder(root);
+    }
+  }
 
  private:
-  bool find(const Type &x, BinaryNode *t) const;
-  void insert(const Type &x, BinaryNode *&t);
-  void remove(const Type &x, BinaryNode *&t);
-  // void makeEmpty(BinaryNode *&t);
+  void midOrder(BinaryNode *t) const {
+    if (t != NULL) {
+      midOrder(t->left);       //先中序遍历左子树
+      cout << t->data << ' ';  //然后遍历左子树
+      midOrder(t->right);      //最后中序遍历左子树
+    }
+  }
 };
 
 template <class Type>
 bool BinarySearchTree<Type>::find(const Type &x) const {
-  return find(x, root);
-}
-
-template <class Type>
-bool BinarySearchTree<Type>::find(const Type &x, BinaryNode *t) const {
-  if (t == NULL)
-    return false;  //查找指针为空，返回未找到
-  else if (x < t->data)
-    return find(x, t->left);  //<x，查找左子树
-  else if (t->data < x)
-    return find(x, t->right);  //>x，查找右子树
-  else
-    return true;  //查找指针的值等于x，返回找到
+  BinaryNode *t = root;
+  cout << "FIND " << x << endl;
+  // cout << root << '\t' << root->data << endl;
+  while (t) {
+    // cout << "\nDEBUG\t" << t->data;
+    if (x < t->data) {
+      t = t->left;  // x在t左侧，查找左子树
+    } else if (x > t->data) {
+      t = t->right;  // x在t右侧，查找右子树
+    } else {
+      return true;  // 查找指针的值等于x，返回找到
+    }
+    // cout << '\t' << t->data << endl;
+  }
+  return false;  // 查找指针为空，返回未找到
 }
 
 template <class Type>
 void BinarySearchTree<Type>::insert(const Type &x) {
-  insert(x, root);
-}
-
-template <class Type>
-void BinarySearchTree<Type>::insert(const Type &x, BinaryNode *&t) {
-  if (t == NULL)  //查找指针为空，新建节点，插入到t位置
-    t = new BinaryNode(x, NULL, NULL);
-  else if (x < t->data)
-    insert(x, t->left);  //>x，在其左子树插入
-  else if (t->data < x)
-    insert(x, t->right);  //<x，在其右子树插入
+  // cout << "INSERT\t";
+  if (root) {
+    BinaryNode *t = root;
+    while (t) {
+      if (x < t->data) {
+        // x在t左侧，在其左子树插入
+        if (t->left) {
+          t = t->left;
+        } else {
+          // 查找指针为空，新建节点，插入到t->left位置
+          t->left = new BinaryNode(x, NULL, NULL);
+          // cout << t->left->left << '\t' << t->left->right << endl;
+          return;
+        }
+      } else if (x > t->data) {
+        // x在t右侧，在其右子树插入
+        if (t->right) {
+          t = t->right;
+        } else {
+          // 查找指针为空，新建节点，插入到t->right位置
+          t->right = new BinaryNode(x, NULL, NULL);
+          // cout << t->right->left << '\t' << t->right->right << endl;
+          return;
+        }
+      } else {
+        return;  // 指针的值等于x，不必插入
+      }
+    }
+  } else {
+    root = new BinaryNode(x, NULL, NULL);
+    // cout << root->left << '\t' << root->right << endl;
+    return;
+  }
 }
 
 template <class Type>
 void BinarySearchTree<Type>::remove(const Type &x) {
-  remove(x, root);
-}
-
-template <class Type>
-void BinarySearchTree<Type>::remove(const Type &x, BinaryNode *&t) {
-  if (t == NULL) return;  //指针为空，返回，没有找到x
-  if (x < t->data)
-    remove(x, t->left);  //>x，在其左子树删除
-  else if (t->data < x)
-    remove(x, t->right);                           //<x，在其右子树删除
-  else if (t->left != NULL && t->right != NULL) {  // t有两个儿子
-    BinaryNode *tmp = t->right;                    // tmp指向t的右子树
-    while (tmp->left != NULL) tmp = tmp->left;     //右子树最小节点
-    t->data = tmp->data;  //把右子树最小节点复制到t节点
-    remove(t->data, t->right);
-  }  //递归删除t右子树的最小节点
-  else {
-    BinaryNode *oldNode = t;  //处理t只有一个儿子的情况
-    t = (t->left != NULL) ? t->left : t->right;
-    delete oldNode;
+  BinaryNode *t = root;
+  while (t) {
+    if (x < t->data) {
+      t = t->left;  // x在t左侧，在其左子树删除
+    } else if (x > t->data) {
+      t = t->right;  // x在t右侧，在其右子树删除
+    } else {
+      // 指针的值等于x
+      if (t->left != NULL && t->right != NULL) {
+        // t有两个儿子
+        BinaryNode *tmp = t->right;  // tmp指向t的右子树
+        while (tmp->left != NULL) {
+          tmp = tmp->left;  // 右子树最小节点
+        }
+        t->data = tmp->data;  // 把右子树最小节点复制到t节点
+        // 删除t右子树的最小节点
+        BinaryNode *oldNode = tmp;
+        tmp = tmp->right;
+        delete oldNode;
+      } else {
+        // 处理t只有一个儿子的情况
+        BinaryNode *oldNode = t;
+        t = (t->left != NULL) ? t->left : t->right;
+        delete oldNode;
+      }
+    }
   }
+  // 指针为空，返回，没有找到x
+  cout << "delete 2 complete" << endl;
 }
--- 8_2,3,9/main.cpp
+++ 8_2,3,9/main.cpp
@@ -10,27 +10,30 @@ int main() {
   for (int i = 0; i < 17; ++i) {
     tree.insert(a[i]);
   }
+
+  tree.midOrder();
   cout << endl;
   cout << "find 2 is " << (tree.find(2) ? "true" : "false") << endl;
   tree.remove(2);
-  cout << "after delete 2, find 2 is " << (tree.find(2) ? "true" : "false")
-       << endl;
+  tree.midOrder();
+  // cout << "after delete 2, find 2 is " << (tree.find(2) ? "true" : "false")
+  //      << endl;
 
-  cout << endl;
-  cout << "find 3 is " << (tree.find(3) ? "true" : "false") << endl;
-  tree.remove(3);
-  cout << "after delete 3, find 3 is " << (tree.find(3) ? "true" : "false")
-       << endl;
+  // cout << endl;
+  // cout << "find 3 is " << (tree.find(3) ? "true" : "false") << endl;
+  // tree.remove(3);
+  // cout << "after delete 3, find 3 is " << (tree.find(3) ? "true" : "false")
+  //      << endl;
 
-  cout << endl;
-  cout << "find 21 is " << (tree.find(21) ? "true" : "false") << endl;
-  tree.remove(21);
-  cout << "after delete 21, find 21 is " << (tree.find(21) ? "true" : "false")
-       << endl;
+  // cout << endl;
+  // cout << "find 21 is " << (tree.find(21) ? "true" : "false") << endl;
+  // tree.remove(21);
+  // cout << "after delete 21, find 21 is " << (tree.find(21) ? "true" : "false")
+  //      << endl;
 
-  cout << endl;
-  cout << "find 1 is " << (tree.find(1) ? "true" : "false") << endl;
-  cout << "find 22 is " << (tree.find(22) ? "true" : "false") << endl;
-  cout << "find 38 is " << (tree.find(38) ? "true" : "false") << endl;
+  // cout << endl;
+  // cout << "find 1 is " << (tree.find(1) ? "true" : "false") << endl;
+  // cout << "find 22 is " << (tree.find(22) ? "true" : "false") << endl;
+  // cout << "find 38 is " << (tree.find(38) ? "true" : "false") << endl;
   return 0;
 }
