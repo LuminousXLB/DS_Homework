68b5ce01c01b80e7ec4a5a03d874f60b15388bc2
LuminousXLB <john980118@163.com>
2017-11-29 22:11:51 +0800

    9-4 completed

--- 9_4/closeHashTable.h
+++ 9_4/closeHashTable.h
@@ -1,121 +1,113 @@
 #pragma once
-#include "hashTable.h"
 #include <iostream>
+#include "hashTable.h"
 
 using namespace std;
 
 template <class Type>
-class closeHashTable: public hashTable<Type> {
-private:
-    struct node {
-        Type data;
-        int state; //0-empty, 1-active, 2-deleted
-        node() {
-            state = 0;
-        }
-    };
-    node *array;
-    int size;
-    int (*key)(const Type& x);
-    int probe(int pos, int i = 0) const {
-        return (pos + 2 * i - 1) % size;
-    }
-public:
-    closeHashTable(int length = 101, int (*f)(const Type &x) = hashTable<Type>::defaultKey);
-    ~closeHashTable() {
-        delete [] array;
-    }
-    int getsize() const {
-        return size;
-    }
-    int find(const Type& x) const;
+class closeHashTable : public hashTable<Type> {
+ private:
+  struct node {
+    Type data;
+    int state;  // 0-empty, 1-active, 2-deleted
+    node() { state = 0; }
+  };
+  node* array;
+  int size;
+  int (*key)(const Type& x);
+  int probe(int pos, int i = 0) const {
+    // 实现平方探测。i恒为0则为线性探测
+    return (pos + 2 * i - 1) % size;
+  }
 
-    // bool find(const Type& x) const;
-    bool insert(const Type& x);
-    bool remove(const Type& x);
-    void rehash(int s = 0);
+ public:
+  closeHashTable(int length = 101,
+                 int (*f)(const Type& x) = hashTable<Type>::defaultKey);
+  ~closeHashTable() { delete[] array; }
+  int getsize() const { return size; }
+  int find(const Type& x) const;
+  // bool find(const Type& x) const;
+  bool insert(const Type& x);
+  bool remove(const Type& x);
+  void rehash(int s = 0);
 };
 
 template <class Type>
-closeHashTable<Type>::closeHashTable(int length, int (*f)(const Type &x)) {
-    size = length;
-    array = new node[size];
-    key = f;
+closeHashTable<Type>::closeHashTable(int length, int (*f)(const Type& x)) {
+  size = length;
+  array = new node[size];
+  key = f;
 }
-
 
 template <class Type>
 bool closeHashTable<Type>::insert(const Type& x) {
-    int initPos, pos;
+  int initPos, pos;
+  // static int conflict_cnt = 0;        // 冲突计数器
+  // static int success_cnt = 0;
+  int cnt = 0;
 
-    // static int conflict_cnt = 0;        // 冲突计数器
-    // static int success_cnt = 0;
-    int cnt = 0;
+  initPos = pos = key(x) % size;
+  do {
+    if (array[pos].state != 1) {
+      array[pos].data = x;
+      array[pos].state = 1;
+      // success_cnt++;
+      // cout << "[SUCCESS] #" << success_cnt
+      //      << "\t key = " << key(x)
+      //      << "\t initPos = " << initPos
+      //      << "\t pos = " << pos << endl;
 
-    initPos = pos = key(x) % size;
-    do {
-        if (array[pos].state != 1) {
-            array[pos].data = x;
-            array[pos].state = 1;
-            // success_cnt++;
-            // cout << "[SUCCESS] #" << success_cnt
-            //      << "\t key = " << key(x)
-            //      << "\t initPos = " << initPos
-            //      << "\t pos = " << pos << endl;
-
-            return true;
-        } else if (array[pos].data == x) {
-            return false;
-        } else {
-            // conflict_cnt++;
-            // cout << "[CONFLICT] #" << conflict_cnt
-            //      << "\t key = " << key(x)
-            //      << "\t initPos = " << initPos
-            //      << "\t pos = " << pos;
-            cnt++;
-            pos = probe(pos, cnt);
-            // cout << "\t nextpos = " << pos << endl;
-        }
-    } while (pos != initPos);
-    return false;
+      return true;
+    } else if (array[pos].data == x) {
+      return false;
+    } else {
+      // conflict_cnt++;
+      // cout << "[CONFLICT] #" << conflict_cnt
+      //      << "\t key = " << key(x)
+      //      << "\t initPos = " << initPos
+      //      << "\t pos = " << pos;
+      cnt++;
+      pos = probe(pos, cnt);
+      // cout << "\t nextpos = " << pos << endl;
+    }
+  } while (pos != initPos);
+  return false;
 }
-
 
 template <class Type>
 bool closeHashTable<Type>::remove(const Type& x) {
-    int initPos, pos;
-    int cnt = 0;
+  int initPos, pos;
+  int cnt = 0;
 
-    initPos = pos = key(x) % size; //计算待删除元素的散列地址
-    do {
-        cnt++;
-        if (array[pos].state == 0) {
-            //指定位置为空，失败
-            return false;
-        } else if (array[pos].data == x) {
-            // 指定位置为x
-            if (array[pos].state == 1) {
-                // active, 预删除
-                array[pos].state = 2;
-                return true;
-            } else if (array[pos].state = 2) {
-                // deleted，删除失败
-                return false;
-            }
-        } else {
-            pos = probe(pos, cnt);
-        }
-    } while (pos != initPos);
-    return false;
+  initPos = pos = key(x) % size;  //计算待删除元素的散列地址
+  do {
+    cnt++;
+    if (array[pos].state == 0) {
+      //指定位置为空，失败
+      return false;
+    } else if (array[pos].data == x) {
+      // 指定位置为x
+      if (array[pos].state == 1) {
+        // active, 预删除
+        array[pos].state = 2;
+        return true;
+      } else if (array[pos].state = 2) {
+        // deleted，删除失败
+        return false;
+      }
+    } else {
+      pos = probe(pos, cnt);
+    }
+  } while (pos != initPos);
+  return false;
 }
-
 
 // template <class Type>
 // bool closeHashTable<Type>::find(const Type& x) const {
 //     int initPos, pos;
 //     bool ret = false;
 //     static int cnt = 0;
-//
+
 //     initPos = pos = key(x) % size;
 //     do {
 //         cnt++;
@@ -128,46 +120,44 @@ bool closeHashTable<Type>::remove(const Type& x) {
 //         }
 //         pos = probe(pos, cnt);
 //     } while (pos != initPos);
-//
+
 //     cout << cnt << endl;
 //     return ret;
 // }
+
 template <class Type>
 int closeHashTable<Type>::find(const Type& x) const {
-    int initPos, pos;
-    bool found = false;
-    int cnt = 0;
+  int initPos, pos;
+  bool found = false;
+  int cnt = 0;
 
-    initPos = pos = key(x) % size;
-    do {
-        cnt++;
-        if (array[pos].state == 0) {
-            found = false;
-            break;
-        } else if ((array[pos].state == 1) && (array[pos].data == x)) {
-            found = true;
-            break;
-        }
-        pos = probe(pos, cnt);
-    } while (pos != initPos);
+  initPos = pos = key(x) % size;
+  do {
+    cnt++;
+    if (array[pos].state == 0) {
+      found = false;
+      break;
+    } else if ((array[pos].state == 1) && (array[pos].data == x)) {
+      found = true;
+      break;
+    }
+    pos = probe(pos, cnt);
+  } while (pos != initPos);
 
-    // cout << cnt << endl;
-    return cnt;
+  return cnt;
 }
-
 
 template <class Type>
 void closeHashTable<Type>::rehash(int s) {
-    node *tmp = array;
-
-    if (s) {
-        size = s;
+  node* tmp = array;
+  if (s) {
+    size = s;
+  }
+  array = new node[size];
+  for (int i = 0; i < size; ++i) {
+    if (tmp[i].state == 1) {
+      insert(tmp[i].data);
     }
-    array = new node[size];
-    for (int i = 0; i < size; ++i) {
-        if (tmp[i].state == 1) {
-            insert(tmp[i].data);
-        }
-    }
-    delete [] tmp;
+  }
+  delete[] tmp;
 }
--- 9_4/main.cpp
+++ 9_4/main.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 #include "closeHashTable.h"
 #include "matElem.h"
 #include "utils.h"
@@ -31,122 +31,99 @@ int matelem2key(const matElem<elemType>& elem);
 
 /******************** main ****************************************************/
 
-int main(int argc, char const *argv[]) {
-    // 设定随机数种子
-    srand(time(NULL));
+int main(int argc, char const* argv[]) {
+  // 设定随机数种子
+  srand(time(NULL));
 
-    double avg = 0;
-    int loop = 1000;
-    for (size_t i = 0; i < loop; i++) {
-        // 初始化散列表
-        // int size = 314;
+  double avg = 0;
+  // 初始化散列表
+  // int size = 314;
 
-        int size = tableSize(_Matrix_Rows_, _Matrix_Columns_, _Matrix_Duty_, _Load_Factor_);
-        int elemcnt = 0;
-        closeHashTable<matElem<int>> ht(size, matelem2key);
-        // if (i == 0) {
-        //     cout << "SIZE\t" << size << endl;
-        // }
+  int size =
+      tableSize(_Matrix_Rows_, _Matrix_Columns_, _Matrix_Duty_, _Load_Factor_);
+  int elemcnt = 0;
+  closeHashTable<matElem<int>> ht(size, matelem2key);
 
-        // 生成随机元素并加入散列表
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                int prob = rand() % 100;
-                if (prob == 1) {
-                    ht.insert(matElem<int>(i, j, 10));
-                    elemcnt++;
-                }
-            }
-        }
-
-        ht.rehash(tableSize(elemcnt, _Load_Factor_));
-
-        // cout << "Create End" << endl;
-        // cout << "SIZE\t" << ht.getsize();
-
-        int find_cnt = 0;
-        // for (int i = 0; i < 10000; ++i) {
-        //     matElem<int> tmp = getRandom(_Matrix_Rows_, _Matrix_Columns_);
-        //     // cout << i << '\t' << rec[tmp.row()][tmp.col()] << endl;
-        //     find_cnt += ht.find(tmp);
-        //     // cout << find_cnt << endl;
-        // }
-        for (int i = 0; i < 100; ++i) {
-            for (int j = 0; j < 100; ++j) {
-                matElem<int> tmp(i, j, 10);
-                find_cnt += ht.find(tmp);
-            }
-        }
-        double rate = find_cnt / 10000.00;
-        if (rate < 1.7) {
-            cout << rate << endl;
-        }
-        avg += rate;
+  // 生成随机元素并加入散列表
+  for (int i = 0; i < 100; i++) {
+    for (int j = 0; j < 100; j++) {
+      int prob = rand() % 100;
+      if (prob == 1) {
+        ht.insert(matElem<int>(i, j, 10));
+        elemcnt++;
+      }
     }
-    cout << "avg\t" << avg / loop << endl;
+  }
 
-    return 0;
+  cout << "% of elements: " << elemcnt / 100.0 << "%";
+
+  ht.rehash(tableSize(elemcnt, _Load_Factor_));
+
+  for (int i = 0; i < 100; ++i) {
+    for (int j = 0; j < 100; ++j) {
+      matElem<int> tmp(i, j, 10);
+    }
+  }
+  return 0;
 }
-
 
 /******************** function implementation *********************************/
 
 // 计算哈希表的规模
 int tableSize(const int mr, const int mc, const double md, const double lf) {
-    // 计算有效值个数并保证负载因子不超过lf
-    int si = mr * mc * md / lf;
+  // 计算有效值个数并保证负载因子不超过lf
+  int si = mr * mc * md / lf;
 
-    // 寻找不小于（保证lf）上述结果的最小素数
-    si += !(si % 2);
-    while (!isPrime(si)) {
-        si += 2;
-    }
+  // 寻找不小于（保证lf）上述结果的最小素数
+  si += !(si % 2);
+  while (!isPrime(si)) {
+    si += 2;
+  }
 
-    return si;
+  return si;
 }
-
 
 int tableSize(int origin, const double lf) {
-    origin = origin / lf;
+  // 计算得到是负载因子小于0.5的最大质数作为散列表的长度
+  origin = origin / lf;
 
-    origin += !(origin % 2);
-    while (!isPrime(origin)) {
-        origin += 2;
-    }
+  origin += !(origin % 2);
+  while (!isPrime(origin)) {
+    origin += 2;
+  }
 
-    return origin;
+  return origin;
 }
-
 
 // 哈希函数
 template <class elemType>
 int matelem2key(const matElem<elemType>& elem) {
-    // int hash = elem.row() * 100 + elem.col(); // 2.05588
+  int hash = elem.row() * 100 + elem.col();  // 2.05588
 
-    // int hash = elem.col() * 100 + elem.row();
+  /*****以下是尝试过的其他哈希函数*****/
+  // int hash = elem.col() * 100 + elem.row();
 
-    int hash = elem.row() * 3 + elem.col() / 33; // 2.57826
+  // int hash = elem.row() * 3 + elem.col() / 33;  // 2.57826
 
-    // int hash = 0;
-    // int r = elem.row();
-    // int c = elem.col();
-    //
-    // // double rate = 211.0 / 81;
-    //
-    // do {
-    //     // hash = hash * 100 + (r % 10) * (c % 10);
-    //     hash = hash * 3 + (r % 11) * (c % 11);
-    //     // hash = hash + (r % 10) * (c % 10) * rate;
-    //     r /= 10;
-    //     c /= 10;
-    // } while (r > 0 || c > 0);
+  // int hash = 0;
+  // int r = elem.row();
+  // int c = elem.col();
+  //
+  // // double rate = 211.0 / 81;
+  //
+  // do {
+  //     // hash = hash * 100 + (r % 10) * (c % 10);
+  //     hash = hash * 3 + (r % 11) * (c % 11);
+  //     // hash = hash + (r % 10) * (c % 10) * rate;
+  //     r /= 10;
+  //     c /= 10;
+  // } while (r > 0 || c > 0);
 
+  // int hash1 = elem.row() * 100 + elem.col();
+  // int hash2 = elem.col() * 100 + elem.row();
+  // int hash = (hash1 * hash2) % (hash1 + hash2);
 
-    // int hash1 = elem.row() * 100 + elem.col();
-    // int hash2 = elem.col() * 100 + elem.row();
-    // int hash = (hash1 * hash2) % (hash1 + hash2);
+  // int hash = elem.row() * elem.row() + elem.col() * elem.col();
 
-    // int hash = elem.row() * elem.row() + elem.col() * elem.col();
-
-    return hash;
+  return hash;
 }
