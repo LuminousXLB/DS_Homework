4848ab38f14413f70aeeb0028cae953b01a5819d
LuminousXLB <john980118@163.com>
2017-11-29 21:37:05 +0800

    9-5

--- 9_5/hashTable.h
+++ 9_5/hashTable.h
@@ -1,16 +1,13 @@
-#pragma once
-
 template <class Type>
 class hashTable {
-public:
-    virtual bool find(const Type& x) const = 0;
-    virtual bool insert(const Type& x) = 0;
-    virtual bool remove(const Type& x) = 0;
+ public:
+  virtual bool find(const Type &x) const = 0;
+  virtual bool insert(const Type &x) = 0;
+  virtual bool remove(const Type &x) = 0;
 
-protected:
-    int (*key)(const Type& x);   //取出元素x的值，并转换成整型
-    static int defaultKey(const int& k) {
-        //如果输入的Key原本就是整形，使用缺省转换函数
-        return k;
-    }
+ protected:
+  int (*key)(const Type &x);  //取出元素x的值，并转换成整型
+                              //生成整型的键值
+  static int defaultKey(const int &k) { return k; }
+  //如果输入的Key原本就是整形，使用缺省转换函数
 };
--- 9_5/main.cpp
+++ 9_5/main.cpp
@@ -3,41 +3,13 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
-	// Éè¶¨Ëæ»úÊıÖÖ×Ó
-	// srand(time(NULL));
+size_t key(const int& a) { return a; }
 
+int main(int argc, char const* argv[]) {
+  // è®¾å®šéšæœºæ•°ç§å­
+  // srand(time(NULL));
 
-	closeHashTable<int> ht(size);
+  closeHashTable<int> ht();
 
-
-	// Éú³ÉËæ»úÔªËØ²¢¼ÓÈëÉ¢ÁĞ±í
-	for (int i = 0; j < 100; j++) {
-		int prob = rand() % 100;
-		if (prob == 1) {
-			ht.insert();
-			elemcnt++;
-		}
-	}
-
-	ht.rehash(tableSize(elemcnt, _Load_Factor_));
-
-	// cout << "Create End" << endl;
-	// cout << "SIZE\t" << ht.getsize();
-
-	int find_cnt = 0;
-	// for (int i = 0; i < 10000; ++i) {
-	//     matElem<int> tmp = getRandom(_Matrix_Rows_, _Matrix_Columns_);
-	//     // cout << i << '\t' << rec[tmp.row()][tmp.col()] << endl;
-	//     find_cnt += ht.find(tmp);
-	//     // cout << find_cnt << endl;
-	// }
-	for (int i = 0; i < 100; ++i) {
-		for (int j = 0; j < 100; ++j) {
-			matElem<int> tmp(i, j, 10);
-			find_cnt += ht.find(tmp);
-		}
-	}
-
-	return 0;
+  return 0;
 }
